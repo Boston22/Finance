01bca9ca73be0e83e10508e126f82048339d74bf
Boston22 <csacadamy@gmail.com>
2022-04-07 10:19:07 -0700

    rg

--- factors.py
+++ factors.py
@@ -6,8 +6,10 @@ app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  
 
 @app.route("/")
 def render_main():
+    type = request.args["year"]
     s = get_state_options()
     y = get_year_options()
+    d = var_data(type)
     return render_template('home.html', state_options = s, year_options = y)
 
 @app.route("/p1")
@@ -34,17 +36,111 @@ def get_state_options():
     return options
 
 def get_year_options():
-    """Return the html code for the drop down menu.  Each option is a state abbreviation from the demographic data."""
+        """Return the html code for the drop down menu.  Each option is a state abbreviation from the demographic data."""
+        with open('finance.json') as demographics_data:
+            counties = json.load(demographics_data)
+        options= Markup("<option value=Capital outlay>Capital outlay </option>" + "<option value=Revenue>Revenue </option>" + "<option value=Expenditure>Expenditure </option>" + "<option value=General expenditure>General expenditure </option>" + "<option value=General revenue>General revenue </option>" + "<option value=Insurance trust revenue>Insurance trust revenue </option>" + "<option value=Intergovernmental>Intergovernmental </option>" + "<option value=License tax>License tax </option>" + "<option value=Selective sales tax>Selective sales tax </option>" + "<option value=Tax>Tax </option>" + "<option value=Debt at end of fiscal year>Debt at end of fiscal year </option>") #Use Markup so <, >, " are not escaped lt, gt, etc.
+        return options
+
+def var_data(type):
     with open('finance.json') as demographics_data:
         counties = json.load(demographics_data)
-    years=[]
-    for c in counties:
-        if c["Year"] not in years:
-            years.append(c["Year"])
-    options=""
-    for y in years:
-        options += Markup("<option value=\"" + str(y) + "\">" + str(y) + "</option>") #Use Markup so <, >, " are not escaped lt, gt, etc.
-    return options
+    #    cpOut = 0
+    #    rev = 0
+    #    exp = 0
+    #    genExp = 0
+    #    genRev = 0
+    #    insTRev = 0
+    #    intgov = 0
+    #    licT = 0
+    #    slcSlT = 0
+    #    t = 0
+    #    dbtFisY = 0
+        for c in counties:
+            if type == "Capital outlay":
+                totals = {}
+                if c["year"] in totals.keys():
+                    totals[c["year"]] += c["totals"]["Capital outlay"]
+                else:
+                    totals[c["year"]] = c["totals"]["Capital outlay"]
+
+            if type == "Revenue":
+                totals = {}
+                if c["year"] in totals.keys():
+                    totals[c["year"]] += c["totals"]["Revenue"]
+                else:
+                    totals[c["year"]] = c["totals"]["Revenue"]
+
+            if type == "Expenditure":
+                totals = {}
+                if c["year"] in totals.keys():
+                    totals[c["year"]] += c["totals"]["Expenditure"]
+                else:
+                    totals[c["year"]] = c["totals"]["Expenditure"]
+
+            if type == "General expenditure":
+                    totals = {}
+                    if c["year"] in totals.keys():
+                        totals[c["year"]] += c["totals"]["General expenditure"]
+                    else:
+                        totals[c["year"]] = c["totals"]["General expenditure"]
+
+            if type == "General revenue":
+                totals = {}
+                if c["year"] in totals.keys():
+                    totals[c["year"]] += c["totals"]["General revenue"]
+                else:
+                    totals[c["year"]] = c["totals"]["General revenue"]
+
+            if type == "Insurance trust revenue":
+                totals = {}
+                if c["year"] in totals.keys():
+                    totals[c["year"]] += c["totals"]["Insurance trust revenue"]
+                else:
+                    totals[c["year"]] = c["totals"]["Insurance trust revenue"]
+
+            if type == "Intergovernmental":
+                totals = {}
+                if c["year"] in totals.keys():
+                    totals[c["year"]] += c["totals"]["Intergovernmental"]
+                else:
+                    totals[c["year"]] = c["totals"]["Intergovernmental"]
+
+            if type == "License tax":
+                totals = {}
+                if c["year"] in totals.keys():
+                    totals[c["year"]] += c["totals"]["License tax"]
+                else:
+                    totals[c["year"]] = c["totals"]["License tax"]
+
+            if type == "Selective sales tax":
+                totals = {}
+                if c["year"] in totals.keys():
+                    totals[c["year"]] += c["totals"]["Selective sales tax"]
+                else:
+                    totals[c["year"]] = c["totals"]["Selective sales tax"]
+
+            if type == "Tax":
+                totals = {}
+                if c["year"] in totals.keys():
+                    totals[c["year"]] += c["totals"]["Tax"]
+                else:
+                    totals[c["year"]] = c["totals"]["Tax"]
+
+            if type == "Debt at end of fiscal year":
+                totals = {}
+                if c["year"] in totals.keys():
+                    totals[c["year"]] += c["totals"]["Debt at end of fiscal year"]
+                else:
+                    totals[c["year"]] = c["totals"]["Debt at end of fiscal year"]
+
+
+    data = ""
+    for a in totals:
+        data += Markup("")
+    return data
+
+
 
 
 
